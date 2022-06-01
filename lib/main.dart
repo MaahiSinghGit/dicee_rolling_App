f0bd2f2a217059b14a2686743b06ae69a77563d8
Maahisinghgit <maahisingh07m@gmail.com>
2022-06-01 19:15:58 +0530

    use Rnadom().nextInt(max) funtion to generate random number in dice

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int left = 1;
   int right = 2;
+  void change(){
+    setState(() {
+      //random generate non negative number form 0 to range max
+      //syntax nextInt(int max);
+      left=Random().nextInt(6)+1;
+      right= Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -34,11 +42,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               // () {} anonymous function
               onPressed: () {
-                setState(() {
-                  //random generate non negative number form 0 to range max
-                  //syntax nextInt(int max);
-                  left=Random().nextInt(6)+1;
-                });
+                change();
               },
               child: Image.asset('Images/dice$left.png'),
             ),
@@ -47,9 +51,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               // () {} is a anonymous function
               onPressed: () {
-                setState(() {
-                  right= Random().nextInt(6)+1;
-                });
+                change();
               },
               child: Image.asset('Images/dice$right.png'),
             ),
