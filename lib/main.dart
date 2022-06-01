0d78a776c5f21b27dd606cf97c2984844d0138a2
Maahisinghgit <maahisingh07m@gmail.com>
2022-06-01 19:16:50 +0530

    all jobs done and project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+
 void main() {
   runApp(
     MaterialApp(
@@ -25,14 +26,15 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int left = 1;
   int right = 2;
-  void change(){
+  void change() {
     setState(() {
       //random generate non negative number form 0 to range max
       //syntax nextInt(int max);
-      left=Random().nextInt(6)+1;
-      right= Random().nextInt(6)+1;
+      left = Random().nextInt(6) + 1;
+      right = Random().nextInt(6) + 1;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Center(
